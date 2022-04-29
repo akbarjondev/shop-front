55e8ccdb41f2ec755fcd08f6055e5ccf1acdc1a3
akbarjondev <tojiyev.akbarjon@gmail.com>
2022-04-29 05:25:06 +0500

    Add notifief to PLP

--- src/components/Card/Card.component.js
+++ src/components/Card/Card.component.js
@@ -2,10 +2,15 @@ import React from "react";
 import StyledCard from "./Card.style";
 import { connect } from "react-redux";
 import { aSelectProduct } from "./../../store/common.actions";
+import Notifier from "../Notifier/Notifier.component";
 
 class Card extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      openNotifier: false,
+    };
 
     this.addToCart = this.addToCart.bind(this);
   }
@@ -39,36 +44,49 @@ class Card extends React.Component {
       inCart: true,
       attributes: selectedAttributes,
     });
+
+    this.setState({
+      openNotifier: true,
+    });
+
+    setTimeout(() => {
+      this.setState({
+        openNotifier: false,
+      });
+    }, 3000);
   }
 
   render() {
     let { product, price } = this.props;
 
     return (
-      <StyledCard inStock={product.inStock}>
-        <div className="card__info">
-          <h3 className="card__title">{product.name}</h3>
-          <div className="card__price">{price}</div>
-        </div>
-        <div className="card__top">
-          <img
-            className="card__image"
-            src={product.gallery[0]}
-            alt={product.title}
-            width={354}
-            height={330}
-          />
-          {product.inStock ? (
-            <button
-              className="card__cart-btn"
-              onClick={(e) => this.addToCart(e)}
-            >
-              <span className="visually-hidden">Add item to cart</span>
-            </button>
-          ) : null}
-          <div className="card__out-of-stock">Out of stock</div>
-        </div>
-      </StyledCard>
+      <>
+        <Notifier open={this.state.openNotifier} />
+        <StyledCard inStock={product.inStock}>
+          <div className="card__info">
+            <h3 className="card__title">{product.name}</h3>
+            <div className="card__price">{price}</div>
+          </div>
+          <div className="card__top">
+            <img
+              className="card__image"
+              src={product.gallery[0]}
+              alt={product.title}
+              width={354}
+              height={330}
+            />
+            {product.inStock ? (
+              <button
+                className="card__cart-btn"
+                onClick={(e) => this.addToCart(e)}
+              >
+                <span className="visually-hidden">Add item to cart</span>
+              </button>
+            ) : null}
+            <div className="card__out-of-stock">Out of stock</div>
+          </div>
+        </StyledCard>
+      </>
     );
   }
 }
